c4c03b871d357aa7ff71decc0a4ea9a3c7c60cfe
Jamie Reep <jareep@gmail.com>
2018-12-31 03:11:13 -0500

    Update compfollowers.py
    Moved the comparison code into a user-defined function

--- compfollowers.py
+++ compfollowers.py
@@ -25,44 +25,47 @@ def main(argv):
       print 'usage: compfollowers.py -1 <file1> -2 <file2>'
       sys.exit()
 
+   comparefiles()
+
+def comparefiles():
+	# Read file into an array of lines
+	db_file = open(file1, "r")
+	dblines = db_file.readlines()
+	db_file.close()
+
+	# Read secondary file into an array of lines
+	go_file = open(file2, "r")
+	golines = go_file.readlines()
+	go_file.close()
+
+	# Initialize loop counters, etc.
+	x = 0
+	y = 0
+	match = 0
+
+	# Start loop of file1
+	while x < len(dblines):
+
+		# Strip carriage return from the lines
+		db_line = dblines[x].strip()
+		x += 1
+
+		# Start loop of file2
+		while y < len(golines):
+			go_line = golines[y].strip()
+			
+			# Check for a match and increase match count if found
+			if db_line == go_line:
+				match += 1
+
+			y += 1
+		
+		# Reset loop counter for 2nd file
+		# (we want to check the entire file each time)
+		y = 0
+
+	# Output our result
+	print match
+
 if __name__ == "__main__":
    main(sys.argv[1:])
-
-# Read file into an array of lines
-db_file = open(file1, "r")
-dblines = db_file.readlines()
-db_file.close()
-
-# Read secondary file into an array of lines
-go_file = open(file2, "r")
-golines = go_file.readlines()
-go_file.close()
-
-# Initialize loop counters, etc.
-x = 0
-y = 0
-match = 0
-
-# Start loop of file1
-while x < len(dblines):
-
-	# Strip carriage return from the lines
-	db_line = dblines[x].strip()
-	x += 1
-
-	# Start loop of file2
-	while y < len(golines):
-		go_line = golines[y].strip()
-		
-		# Check for a match and increase match count if found
-		if db_line == go_line:
-			match += 1
-
-		y += 1
-	
-	# Reset loop counter for 2nd file
-	# (we want to check the entire file each time)
-	y = 0
-
-# Output our result
-print match
